b58f14e0a74eae16f954cfc8a2be40e6cfba4240
rethfupy <solamdev@outlook.com>
2023-03-06 14:24:09 +0200

    Initial commit

---
+++ GetSolution/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace GetSolution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelTop = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.labelA = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.labelC = new System.Windows.Forms.Label();
+            this.textboxA = new System.Windows.Forms.TextBox();
+            this.textboxB = new System.Windows.Forms.TextBox();
+            this.textboxC = new System.Windows.Forms.TextBox();
+            this.buttonSum = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.labelBot = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelTop
+            // 
+            this.labelTop.BackColor = System.Drawing.Color.Transparent;
+            this.labelTop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTop.Location = new System.Drawing.Point(12, 45);
+            this.labelTop.Name = "labelTop";
+            this.labelTop.Size = new System.Drawing.Size(435, 54);
+            this.labelTop.TabIndex = 0;
+            this.labelTop.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(199, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 136);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор типа уравнения:";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(28, 46);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(204, 22);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Квадратное уравнение";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(28, 86);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(187, 22);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Линейное уравнение";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.BackColor = System.Drawing.Color.Transparent;
+            this.labelA.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelA.Location = new System.Drawing.Point(13, 122);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(27, 18);
+            this.labelA.TabIndex = 2;
+            this.labelA.Text = "a :";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.BackColor = System.Drawing.Color.Transparent;
+            this.labelB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelB.Location = new System.Drawing.Point(13, 164);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(27, 18);
+            this.labelB.TabIndex = 3;
+            this.labelB.Text = "b :";
+            // 
+            // labelC
+            // 
+            this.labelC.AutoSize = true;
+            this.labelC.BackColor = System.Drawing.Color.Transparent;
+            this.labelC.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelC.Location = new System.Drawing.Point(13, 205);
+            this.labelC.Name = "labelC";
+            this.labelC.Size = new System.Drawing.Size(27, 18);
+            this.labelC.TabIndex = 4;
+            this.labelC.Text = "c :";
+            // 
+            // textboxA
+            // 
+            this.textboxA.Location = new System.Drawing.Point(46, 123);
+            this.textboxA.Name = "textboxA";
+            this.textboxA.Size = new System.Drawing.Size(132, 20);
+            this.textboxA.TabIndex = 5;
+            this.textboxA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textboxA_KeyPress);
+            this.textboxA.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textboxA_KeyUp);
+            this.textboxA.Validating += new System.ComponentModel.CancelEventHandler(this.textboxA_Validating);
+            // 
+            // textboxB
+            // 
+            this.textboxB.Location = new System.Drawing.Point(46, 165);
+            this.textboxB.Name = "textboxB";
+            this.textboxB.Size = new System.Drawing.Size(132, 20);
+            this.textboxB.TabIndex = 6;
+            this.textboxB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textboxA_KeyPress);
+            this.textboxB.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textboxA_KeyUp);
+            this.textboxB.Validating += new System.ComponentModel.CancelEventHandler(this.textboxA_Validating);
+            // 
+            // textboxC
+            // 
+            this.textboxC.Location = new System.Drawing.Point(46, 206);
+            this.textboxC.Name = "textboxC";
+            this.textboxC.Size = new System.Drawing.Size(132, 20);
+            this.textboxC.TabIndex = 7;
+            this.textboxC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textboxA_KeyPress);
+            this.textboxC.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textboxA_KeyUp);
+            this.textboxC.Validating += new System.ComponentModel.CancelEventHandler(this.textboxA_Validating);
+            // 
+            // buttonSum
+            // 
+            this.buttonSum.Enabled = false;
+            this.buttonSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSum.Location = new System.Drawing.Point(67, 288);
+            this.buttonSum.Name = "buttonSum";
+            this.buttonSum.Size = new System.Drawing.Size(102, 28);
+            this.buttonSum.TabIndex = 8;
+            this.buttonSum.Text = "Вычислить";
+            this.buttonSum.UseVisualStyleBackColor = true;
+            this.buttonSum.Click += new System.EventHandler(this.buttonSum_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClear.Location = new System.Drawing.Point(199, 288);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(102, 28);
+            this.buttonClear.TabIndex = 9;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // labelBot
+            // 
+            this.labelBot.BackColor = System.Drawing.Color.Transparent;
+            this.labelBot.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelBot.Location = new System.Drawing.Point(12, 356);
+            this.labelBot.Name = "labelBot";
+            this.labelBot.Size = new System.Drawing.Size(435, 54);
+            this.labelBot.TabIndex = 10;
+            this.labelBot.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenu,
+            this.infoMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(459, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileMenu
+            // 
+            this.fileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearMenu,
+            this.toolStripSeparator1,
+            this.exitMenu});
+            this.fileMenu.Name = "fileMenu";
+            this.fileMenu.Size = new System.Drawing.Size(48, 20);
+            this.fileMenu.Text = "&Файл";
+            // 
+            // clearMenu
+            // 
+            this.clearMenu.Name = "clearMenu";
+            this.clearMenu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.C)));
+            this.clearMenu.Size = new System.Drawing.Size(164, 22);
+            this.clearMenu.Text = "&Очистить";
+            this.clearMenu.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(161, 6);
+            // 
+            // exitMenu
+            // 
+            this.exitMenu.Name = "exitMenu";
+            this.exitMenu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.X)));
+            this.exitMenu.Size = new System.Drawing.Size(164, 22);
+            this.exitMenu.Text = "&Выход";
+            this.exitMenu.Click += new System.EventHandler(this.exitMenu_Click);
+            // 
+            // infoMenu
+            // 
+            this.infoMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutMenu});
+            this.infoMenu.Name = "infoMenu";
+            this.infoMenu.Size = new System.Drawing.Size(51, 20);
+            this.infoMenu.Text = "Инфо";
+            // 
+            // aboutMenu
+            // 
+            this.aboutMenu.Name = "aboutMenu";
+            this.aboutMenu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.I)));
+            this.aboutMenu.Size = new System.Drawing.Size(182, 22);
+            this.aboutMenu.Text = "&О программе";
+            this.aboutMenu.Click += new System.EventHandler(this.aboutMenu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GetSolution.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(459, 450);
+            this.Controls.Add(this.labelBot);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSum);
+            this.Controls.Add(this.textboxC);
+            this.Controls.Add(this.textboxB);
+            this.Controls.Add(this.textboxA);
+            this.Controls.Add(this.labelC);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelTop);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = " ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTop;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.Label labelC;
+        private System.Windows.Forms.TextBox textboxA;
+        private System.Windows.Forms.TextBox textboxB;
+        private System.Windows.Forms.TextBox textboxC;
+        private System.Windows.Forms.Button buttonSum;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label labelBot;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMenu;
+        private System.Windows.Forms.ToolStripMenuItem clearMenu;
+        private System.Windows.Forms.ToolStripMenuItem infoMenu;
+        private System.Windows.Forms.ToolStripMenuItem aboutMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitMenu;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ GetSolution/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetSolution
+{
+    public partial class Form1 : Form
+    {
+        string[] text =
+        {
+            "Нахождение корней квадратного уравнения",
+            "Нахождение корней линейнего уравнения"
+        };
+
+        string[] textLabel =
+        {
+            "ax² + bx + c = 0",
+            "bx + c = 0"
+        };
+
+        int flag = 0;
+
+        ErrorProvider errorProvider;
+
+        char sep = System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator[0];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = text[flag];
+
+            this.labelTop.Text = textLabel[flag];
+
+            this.textboxA.Select(); // выделенный текстбокс А
+            this.CenterToScreen(); // приложение в центре экрана
+
+            errorProvider = new ErrorProvider();
+            errorProvider.BlinkRate = 500;
+            errorProvider.BlinkStyle = ErrorBlinkStyle.AlwaysBlink;
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+            MaximizeBox = false;
+
+            textboxA.MaxLength = 20;
+            textboxB.MaxLength = 20;
+            textboxC.MaxLength = 20;
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rbt = (RadioButton)sender;
+            if (rbt.Checked)
+            {
+                if (rbt == radioButton1)
+                {
+                    flag = 1;
+                    //textboxA.Text = "";
+                    //textboxA.Enabled = false;
+
+                    this.labelA.Visible = false;
+                    this.textboxA.Visible = false;
+
+                    this.textboxB.Select();
+                }
+                else
+                {
+                    flag = 0;
+                    //textboxA.Enabled = true;
+
+                    this.labelA.Visible = true;
+                    this.textboxA.Visible = true;
+
+                    this.textboxA.Select();
+                }
+
+                this.Text = text[flag];
+
+                this.labelTop.Text = textLabel[flag];
+
+                errorProvider.SetError(textboxA, "");
+                errorProvider.SetError(textboxB, "");
+                errorProvider.SetError(textboxC, "");
+            }
+        }
+
+        private void textboxA_Validating(object sender, CancelEventArgs e)
+        {
+            if ((TextBox)sender == this.textboxA)
+            {
+                try
+                {
+                    if (Double.Parse(this.textboxA.Text) == 0)
+                    {
+                        errorProvider.SetError(this.textboxA, "Cannot be 0!");
+                    }
+                    else
+                    {
+                        errorProvider.SetError(this.textboxA, "");
+                    }
+                }
+                catch (Exception exc)
+                {
+                    errorProvider.SetError(this.textboxA, exc.Message);
+                }
+
+            }
+            else
+            {
+                try
+                {
+                    double d = Double.Parse(((TextBox)sender).Text);
+                    errorProvider.SetError((TextBox)sender, "");
+                }
+                catch (Exception exc)
+                {
+                    errorProvider.SetError((TextBox)sender, exc.Message);
+                }
+            }
+        }
+
+        private void textboxA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (ch == '.' || ch == ',')
+                e.KeyChar = ch = sep;
+
+            // контроль правильности вводимых данных
+            if (!Char.IsDigit(ch) && !(ch == 8) &&
+                (ch != sep || ((TextBox)sender).Text.IndexOf(sep) != -1 || ((TextBox)sender).Text.Length == 0) &&
+                (ch != '-' || ((TextBox)sender).Text.Length > 0)
+                )
+            {
+                e.Handled = true;
+            }
+
+            // при нажатии Esc текущее поле очищается
+            if(ch == 27)
+            {
+                ((TextBox)sender).Text = "";
+            }
+
+            // при нажатии Enter переход к следующему полю
+            else if(ch == 13)
+            {
+                if((TextBox)sender == this.textboxA)
+                {
+                    this.textboxB.Focus();
+                }
+                else if ((TextBox)sender == this.textboxB)
+                {
+                    this.textboxC.Focus();
+                }
+                else if ((TextBox)sender == this.textboxC)
+                {
+                    // кое что сюда добавим
+
+                    if (buttonSum.Enabled)
+                    {
+                        buttonSum.Focus();
+                        InvokeOnClick(buttonSum, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+        private void textboxA_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(flag == 0)
+            {
+                buttonSum.Enabled = (
+                    textboxA.Text != "0" &&
+                    textboxA.Text != "" && textboxA.Text != "-" &&
+                    textboxA.Text != sep.ToString() &&
+
+                    textboxB.Text != "" && textboxB.Text != "-" &&
+                    textboxB.Text != sep.ToString() &&
+
+                    textboxC.Text != "" && textboxC.Text != "-" &&
+                    textboxC.Text != sep.ToString())
+                    ? true : false;
+            }
+            else
+            {
+                buttonSum.Enabled = (
+                    textboxB.Text != "" && textboxB.Text != "-" &&
+                    textboxB.Text != sep.ToString() &&
+
+                    textboxC.Text != "" && textboxC.Text != "-" &&
+                    textboxC.Text != sep.ToString())
+                    ? true : false;
+            }
+        }
+
+        private void buttonSum_Click(object sender, EventArgs e)
+        {
+            if (buttonSum.Enabled)
+            {
+                if (flag == 00)
+                {
+                    double a, b, c;
+                    a = Double.Parse(textboxA.Text);
+                    b = Double.Parse(textboxB.Text);
+                    c = Double.Parse(textboxC.Text);
+
+                    double x1, x2;
+
+                    int res = GetSolution(out x1, out x2, a, b, c);
+
+                    switch (res)
+                    {
+                        case 2:
+                            labelBot.Text = "x1 = " + x1 + ", x2 = " + x2;
+                            //labelBot.Text = String.Format("x1 = {0}, x2 = {0}", x1, x2);
+                            break;
+                        case 1:
+                            labelBot.Text = "x1 = x2 = " + x1;
+                            break;
+                        case 0:
+                            labelBot.Text = "Нет корней уравнения!";
+                            break;
+                        case -1:
+                            labelBot.Text = "Error  a==0!!!";
+                            break;
+                    }
+
+                }
+                else
+                {
+                    double b, c;
+                    b = Double.Parse(textboxB.Text);
+                    c = Double.Parse(textboxC.Text);
+
+                    double x;
+                    int res = GetLinear(out x, b, c);
+
+                    switch (res)
+                    {
+                        case 1:
+                            labelBot.Text = "x = " + x;
+                            break;
+                        case 0:
+                            labelBot.Text = "Корень уравнения — любое число";
+                            break;
+                        case -1:
+                            labelBot.Text = "Нет корней уравнения!";
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            errorProvider.SetError(textboxA, "");
+            errorProvider.SetError(textboxB, "");
+            errorProvider.SetError(textboxC, "");
+
+            //textboxA.Text = "";
+            //textboxB.Text = "";
+            //textboxC.Text = "";
+            //labelBot.Text = "";
+
+            textboxA.Text = textboxB.Text = textboxC.Text = labelBot.Text = "";
+            buttonSum.Enabled = false;
+        }
+
+        public static int GetSolution(
+            out double x1, out double x2,
+            double a, double b, double c)
+        {
+            x1 = x2 = 0;
+
+            if (a == 0) { return -1; }
+            double d = b * b - 4 * a * c;
+            if (d > 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                return 2;
+            }
+            if (d == 0)
+            {
+                x1 = x2 = -b / (2 * a); return 1;
+            }
+            return 0;
+        }
+
+        public static int GetLinear(out double x, double b, double c)
+        {
+            x = 0;
+
+            if(b != 0)
+            {
+                x = -c / b;
+                return 1;
+            }
+            if(b == 0 && c != 0)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+        private void exitMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutMenu_Click(object sender, EventArgs e)
+        {
+            AboutBox dlg = new AboutBox();
+            dlg.Text = text[flag];
+            dlg.ShowDialog();
+        }
+    }
+}
